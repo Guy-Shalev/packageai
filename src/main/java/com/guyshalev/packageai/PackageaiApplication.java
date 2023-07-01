f4f396b162b40de67767361d02c470f1954dab6a
Guy-Shalev <guy.shalev.email@gmail.com>
2023-07-01 08:48:50 +0300

    Initial commit

---
+++ src/main/java/com/guyshalev/packageai/PackageaiApplication.java
@@ -0,0 +1,13 @@
+package com.guyshalev.packageai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guyshalev/packageai/PackageaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guyshalev.packageai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
